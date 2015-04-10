0b070d3f57ecc4cc20c185548ff13db26bae3f23
Chris-shari <gimlorn.lrt@gmail.com>
2015-04-10 13:33:37 +0200

    Added: #1 Rudimentary WASD-Control
    A little cube can be moved on a plane by using WASD on the keyboard. The
    sidewalls have colliders.

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+
+    void FixedUpdate()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        float turn = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        move(moveVertical);
+
+        rotate(turn);
+
+
+
+
+
+        
+
+        
+    }
+
+    private void rotate(float turn)
+    {
+        float rotationSpeed = 80f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void move(float input)
+    {
+        float movementSpeed = 5f;
+
+        rb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime * input);
+    }
+}
