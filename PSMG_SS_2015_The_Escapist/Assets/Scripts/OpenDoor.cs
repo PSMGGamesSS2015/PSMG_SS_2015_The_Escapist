46ad47eb4cbd733341cf3caa5212b81a385a7f4b
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-04-22 00:22:52 +0200

    Fixed: Corrupted Scene

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+    private GameObject player;
+    private Animation openAnim;
+    private float openAnimTime = 1f;
+    private float waitForCloseTime = 1f;
+    private float nextTriggerTime;
+    private bool doorOpen = false;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        openAnim = GetComponent<Animation>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            StopCoroutine("triggerCloseDoor");
+
+            if (Input.GetButton("Use"))
+            {
+                if (!doorOpen) openDoor(); else closeDoor();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player && doorOpen)
+        {
+            StartCoroutine("triggerCloseDoor");
+        }
+    }
+
+    void openDoor()
+    {
+        if (Time.time > nextTriggerTime)
+        {
+            openAnim["openDoor"].speed = 1;
+            openAnim["openDoor"].time = 0;
+
+            openAnim.Play();
+
+            doorOpen = true;
+            nextTriggerTime = Time.time + openAnimTime;
+        }
+    }
+
+    void closeDoor()
+    {
+        if (Time.time > nextTriggerTime)
+        {
+            openAnim["openDoor"].speed = -1;
+            openAnim["openDoor"].time = openAnim["openDoor"].length;
+
+            GetComponent<Animation>().Play();
+
+            doorOpen = false;
+            nextTriggerTime = Time.time + openAnimTime;
+        }
+    }
+
+    IEnumerator triggerCloseDoor()
+    {
+        float timeToWait = (Time.time < nextTriggerTime) ? (nextTriggerTime - Time.time) + waitForCloseTime : waitForCloseTime;
+        yield return new WaitForSeconds(timeToWait);
+
+        closeDoor();
+    }
+}
