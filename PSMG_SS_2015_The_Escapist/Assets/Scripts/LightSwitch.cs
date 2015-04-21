ab423007cfd5cc681541ab2351e0e8244395ab6f
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-04-21 02:26:00 +0200

    Added: #11 Light Switch

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour 
+{
+    public Light light1;
+    public Light light2;
+
+    private float switchAnimTime = 0.5f;
+    private float nextSwitchTime;
+    private float nextRotation = -45;
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (Input.GetButton("Use"))
+            {
+                SwitchLight();
+            }
+        }
+    }
+
+    void SwitchLight()
+    {
+        if (Time.time > nextSwitchTime)
+        {
+            light1.enabled = !light1.enabled;
+            light2.enabled = !light2.enabled;
+            transform.rotation = Quaternion.Euler(nextRotation, 0, 0);
+            nextSwitchTime = Time.time + switchAnimTime;
+            nextRotation *= -1;
+        }
+        
+
+    }
+}
