244ef1a11d5ff059c28bf3d46111d1692adeee52
Toby3011 <tobiaszirngibl@gmx.de>
2015-04-21 19:11:28 +0200

    Added: #20 Hiding in shadows
    Player can now hide in shadows

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/Shadow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shadow : MonoBehaviour
+{
+
+    public GameObject player;
+    public bool isPlayerInLight;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 rayDirection = player.transform.position - transform.position;
+
+        if (Physics.Raycast(transform.position, rayDirection, out hit))
+        {
+
+            if (hit.transform.tag == "Player")
+            {
+
+                isPlayerInLight = true;
+           }
+            else if (hit.transform.tag != "Enemy")
+            {
+                isPlayerInLight = false;
+        }
+    }
+}
+    void OnGUI()
+    {
+        if (isPlayerInLight == false)
+        {
+            GUILayout.Label("Versteckt");
+        }
+    }
+}
