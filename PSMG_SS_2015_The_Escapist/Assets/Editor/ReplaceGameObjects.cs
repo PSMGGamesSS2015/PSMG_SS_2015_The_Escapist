77dbf4d8fa2ea9b96000d633cae73e8d380ad641
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-05-22 00:08:38 +0200

    Fixed: #51 Rebuild first Game Level
    Rebuild the first Game Level with the new divided prefabs. The ground is now independent from the rest.

---
+++ PSMG_SS_2015_The_Escapist/Assets/Editor/ReplaceGameObjects.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+// CopyComponents - by Michael L. Croswell for Colorado Game Coders, LLC
+// March 2010
+//Modified by Kristian Helle Jespersen
+//June 2011
+//Modified by Connor Cadellin McKee for Excamedia
+//April 2015
+//Modified by Fernando Medina (fermmmm)
+//April 2015
+public class ReplaceGameObjects : ScriptableWizard {
+
+    public GameObject Prefab;
+    public GameObject[] ObjectsToReplace;
+    public bool KeepOriginalNames = true;
+    [MenuItem("Custom/Replace GameObjects")]
+
+    static void CreateWizard()
+    {
+        var replaceGameObjects = DisplayWizard<ReplaceGameObjects>("Replace GameObjects", "Replace");
+        replaceGameObjects.ObjectsToReplace = Selection.gameObjects;
+    }
+
+    void OnWizardCreate()
+    {
+        foreach (GameObject go in ObjectsToReplace)
+        {
+            GameObject newObject;
+            newObject = (GameObject)PrefabUtility.InstantiatePrefab(Prefab);
+            newObject.transform.SetParent(go.transform.parent, true);
+            newObject.transform.localPosition = go.transform.localPosition;
+            newObject.transform.localRotation = go.transform.localRotation;
+            newObject.transform.localScale = go.transform.localScale;
+            if (KeepOriginalNames) newObject.transform.name = go.transform.name;
+            DestroyImmediate(go);
+        }
+    }
+}
