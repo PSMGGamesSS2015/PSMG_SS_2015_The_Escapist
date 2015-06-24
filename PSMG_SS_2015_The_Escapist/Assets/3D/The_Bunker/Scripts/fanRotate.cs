ec26a5b093798d117573700000544be6f3c799f0
Bene <Benedikt.Hierl@gmx.de>
2015-06-24 21:06:48 +0200

    Added: #108 The Bunker Levelset - Part 1
    Source: https://www.assetstore.unity3d.com/en/#!/content/19901

---
+++ PSMG_SS_2015_The_Escapist/Assets/3D/The_Bunker/Scripts/fanRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fanRotate : MonoBehaviour {
+
+	public Vector3 rotationSpeed = new Vector3( 0f, 0f, 0f );
+
+	void Update()
+	{
+		transform.Rotate( rotationSpeed * Time.deltaTime );
+	}
+}
---
+++ PSMG_SS_2015_The_Escapist/Assets/3D/The_Bunker/Scripts/lerpedLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lerpedLight : MonoBehaviour {
+
+	public Color distortColor = Color.white;
+	private Color baseColor = Color.white;
+
+	public float blinkFrequency = 1f;
+	private float lerper = 0f;
+	private Light myLight;
+
+	private int direction = 1;
+
+	void Start()
+	{
+		myLight = gameObject.GetComponent< Light >();
+		baseColor = myLight.color;
+	}
+
+	void Update()
+	{
+
+		if( direction > 0f ) lerper += blinkFrequency * Time.deltaTime;
+		else if( direction < 0f ) lerper -= blinkFrequency * Time.deltaTime;
+
+		myLight.color = Color.Lerp( baseColor, distortColor, lerper );
+
+		if( myLight.color == distortColor ) direction = -1;
+		if( myLight.color == baseColor ) direction = 1;
+	}
+}
