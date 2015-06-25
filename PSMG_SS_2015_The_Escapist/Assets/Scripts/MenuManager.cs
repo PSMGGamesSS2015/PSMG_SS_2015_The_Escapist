3e678bdc9d8c1b786e9614d2ce9071bea969f6c3
Toby3011 <tobiaszirngibl@gmx.de>
2015-06-25 22:59:12 +0200

    Updated: #99 Main Menu
    Redesign of Buttons, Enabled all Video Settings

--- PSMG_SS_2015_The_Escapist/Assets/Scripts/MenuManager.cs
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/MenuManager.cs
@@ -14,14 +14,19 @@ public class MenuManager : MonoBehaviour
 
     int fieldOfView = 80;
 
-    Color boxStyle;
+    public int ResX;
+    public int ResY;
 
-    
-    public static bool fullscreen = false; //default...
+
+    private GUIStyle textStyle;
+
+    public bool Fullscreen; //default...
 
     public void Start()
     {
-        
+        textStyle = new GUIStyle();
+
+
     }
 
     // Use this for initialization
@@ -49,19 +54,20 @@ public class MenuManager : MonoBehaviour
     public void OnGUI()
     {
 
+
         if (menu)
         {
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 30), "Start Game"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 70, 200, 80), "Start Game"))
             {
 
                 ChangeScene(1);
-                
+
             }
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 30, 100, 30), "Options"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 80), "Options"))
             {
 
 
@@ -70,7 +76,7 @@ public class MenuManager : MonoBehaviour
             }
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 90, 100, 30), "Quit"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 130, 200, 80), "Quit"))
             {
 
 
@@ -80,7 +86,7 @@ public class MenuManager : MonoBehaviour
         if (optionmenu)
         {
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 30), "Audio Settings"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 70, 200, 80), "Audio Settings"))
             {
 
 
@@ -89,7 +95,7 @@ public class MenuManager : MonoBehaviour
             }
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 30, 100, 30), "Video Settings"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 80), "Video Settings"))
             {
 
 
@@ -98,26 +104,26 @@ public class MenuManager : MonoBehaviour
             }
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 90, 100, 30), "Back"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 130, 200, 80), "Back"))
             {
 
 
                 optionmenu = false;
-                menu = true;    
+                menu = true;
             }
         }
 
-       
+
 
         if (sound)
         {
-            
 
 
-            sfxVol = (int) GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 30), sfxVol, 0.0f, 10.0f);
+
+            sfxVol = (int)GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 30), sfxVol, 0.0f, 10.0f);
             GUI.Label(new Rect(Screen.width / 2 - 50 + 110, Screen.height / 2 - 5, 100, 30), "SFX: " + sfxVol);
 
-            musicVol = (int) GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 30, 100, 30), musicVol, 0.0f, 10.0f);
+            musicVol = (int)GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 30, 100, 30), musicVol, 0.0f, 10.0f);
             GUI.Label(new Rect(Screen.width / 2 - 50 + 110, Screen.height / 2 + 25, 100, 30), "Music: " + musicVol);
 
             if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 90, 100, 30), "Back"))
@@ -127,22 +133,24 @@ public class MenuManager : MonoBehaviour
             }
         }
 
-       
+
 
         if (video)
         {
-           
+
 
 
             string[] qualities = QualitySettings.names;
 
             GUILayout.BeginVertical();
 
+            GUI.Label(new Rect(Screen.width / 2 - 35, Screen.height / 2 - 95, 100, 30), "Video Quality");
+
             for (int i = 0; i < qualities.Length; i++)
             {
 
 
-                if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 40 + i * 30, 100, 30), qualities[i]))
+                if (GUI.Button(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 70 + i * 53, 120, 40), qualities[i]))
                 {
 
 
@@ -152,12 +160,96 @@ public class MenuManager : MonoBehaviour
 
             GUILayout.EndVertical();
 
+            GUI.Label(new Rect(Screen.width / 2 + 95, Screen.height / 2 - 95, 120, 30), "Antialiasing");
 
-            fieldOfView = (int) GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 75, 100, 20), fieldOfView, 30, 120);
-            GUI.Label(new Rect(Screen.width / 2 - 50 + 110, Screen.height / 2 - 80, 100, 30), "FOV: " + fieldOfView);
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 - 70, 120, 40), "No AA"))
+            {
+                QualitySettings.antiAliasing = 0;
+            }
+            //2 X AA SETTINGS
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 - 25, 120, 40), "2x AA"))
+            {
+                QualitySettings.antiAliasing = 2;
+            }
+            //4 X AA SETTINGS
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 + 20, 120, 40), "4x AA"))
+            {
+                QualitySettings.antiAliasing = 4;
+            }
+            //8 x AA SETTINGS
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 + 65, 120, 40), "8x AA"))
+            {
+                QualitySettings.antiAliasing = 8;
+            }
+
+            GUI.Label(new Rect(Screen.width / 2 + 110, Screen.height / 2 + 125, 120, 30), "Vsync");
 
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 180, 100, 30), "Back"))
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 + 150, 120, 40), "Vsync On"))
+            {
+                QualitySettings.vSyncCount = 1;
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 + 70, Screen.height / 2 + 195, 120, 40), "Vsync Off"))
+            {
+                QualitySettings.vSyncCount = 0;
+            }
+
+
+
+            GUI.Label(new Rect(Screen.width / 2 - 167.5f, Screen.height / 2 - 95, 120, 30), "Refresh Rate");
+
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 - 70, 120, 40), "60Hz"))
+            {
+                Screen.SetResolution(ResX, ResY, Fullscreen, 60);
+            }
+            //120Hz
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 - 25, 120, 40), "120Hz"))
+            {
+                Screen.SetResolution(ResX, ResY, Fullscreen, 120);
+            }
+
+            GUI.Label(new Rect(Screen.width / 2 - 160, Screen.height / 2 + 35, 120, 30), "Resolution");
+
+
+            //1080p
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 + 60, 120, 40), "1080p"))
+            {
+                Screen.SetResolution(1920, 1080, Fullscreen);
+                ResX = 1920;
+                ResY = 1080;
+              
+            }
+            //720p
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 + 105, 120, 40), "720p"))
+            {
+                Screen.SetResolution(1280, 720, Fullscreen);
+                ResX = 1280;
+                ResY = 720;
+            
+            }
+            //480p
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 + 150, 120, 40), "480p"))
+            {
+                Screen.SetResolution(640, 480, Fullscreen);
+                ResX = 640;
+                ResY = 480;
+                
+
+            }
+
+            //360p
+            if (GUI.Button(new Rect(Screen.width / 2 - 190, Screen.height / 2 + 195, 120, 40), "360p"))
+            {
+                Screen.SetResolution(640, 360, Fullscreen);
+                ResX = 640;
+                ResY = 360;
+              
+
+            }
+
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 120, Screen.height / 2 + 250, 220, 60), "Back"))
             {
 
 
@@ -165,10 +257,11 @@ public class MenuManager : MonoBehaviour
                 optionmenu = true;
             }
 
-           
+
         }
     }
 }
+
 
             
 
