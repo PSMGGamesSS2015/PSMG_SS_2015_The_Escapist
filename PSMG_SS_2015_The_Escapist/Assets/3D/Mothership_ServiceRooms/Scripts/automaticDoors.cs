f8224ca7d2b7387488e3ec04f3bfb5043fec1730
Bene <Benedikt.Hierl@gmx.de>
2015-06-28 16:39:03 +0200

    Added: #108 Industrial service rooms - Levelkit
    I have added the "Mothership Service-Room" asset pack. This is the only
    one, which could be found in the asset store, which nearly fits our
    requirements for the industrial part in the end of level 2.

---
+++ PSMG_SS_2015_The_Escapist/Assets/3D/Mothership_ServiceRooms/Scripts/automaticDoors.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class automaticDoors : MonoBehaviour {
+
+	public float openSpeed = 5f;
+
+	public List< Light > lights = new List<Light>();
+
+	public Color openedColor = new Color( 1f, 1f, 1f, 1f );
+	public Color closedColor = new Color( 0f, 0f, 0f, 0f );
+
+	public Transform doorsWing;
+	public Vector3 openedPosition = new Vector3( 0f, 0f, 0f );
+	private Vector3 closedPosition = new Vector3( 0f, 0f, 0f );
+
+	private int state = 0; // 0 - closed, 1 - opening, 2 - opened, 3 - closing
+	private float lerper = 0f;
+
+	public bool openable = true;
+
+	public AudioClip openSFX;
+	public AudioClip closeSFX;
+
+	void Start()
+	{
+		closedPosition = doorsWing.transform.localPosition;
+
+		if( openable )
+		{
+			foreach( Light l in lights ) l.color = openedColor;
+		}
+		else if( !openable )
+		{
+			foreach( Light l in lights ) l.color = closedColor;
+		}
+	}
+
+	void Update()
+	{
+		if( state == 0 )
+		{
+			//do nothing
+		}
+		else if( state == 1 )
+		{
+			//opening doors
+			if( lerper < 1f )
+			{
+				lerper += Time.deltaTime * openSpeed;
+				doorsWing.localPosition = Vector3.Lerp( closedPosition, openedPosition, lerper );
+			}
+			else
+			{
+				state = 2;
+				return;
+			}
+		}
+		else if( state == 2 )
+		{
+			//do noting
+		}
+		else if( state == 3 )
+		{
+			//closing doors
+
+			if( lerper > 0f )
+			{
+				lerper -= Time.deltaTime * openSpeed;
+				doorsWing.localPosition = Vector3.Lerp( closedPosition, openedPosition, lerper );
+			}
+			else
+			{
+				state = 0;
+				return;
+			}
+		}
+	}
+
+	public void Open()
+	{
+		if( !openable ) return;
+
+		//lerper = 0f;
+		state = 1;
+
+		if( openSFX ) GetComponent<AudioSource>().PlayOneShot( openSFX );
+	}
+
+	public void Close()
+	{
+		if( !openable ) return;
+
+		//lerper = 1f;
+		state = 3;
+
+		if( closeSFX ) GetComponent<AudioSource>().PlayOneShot( closeSFX );
+	}
+
+	public void setOpenable( bool o )
+	{
+		openable = o;
+
+		if( openable )
+		{
+			foreach( Light l in lights ) l.color = openedColor;
+		}
+		else if( !openable )
+		{
+			foreach( Light l in lights ) l.color = closedColor;
+		}
+	}
+}
---
+++ PSMG_SS_2015_The_Escapist/Assets/3D/Mothership_ServiceRooms/Scripts/automaticDoorsTerminal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class automaticDoorsTerminal : MonoBehaviour {
+
+	public automaticDoors doors;
+
+	void OnTriggerEnter( Collider other )
+	{
+		if( other.tag == "Player" )
+		{
+			doors.Open();
+		}
+	}
+
+	void OnTriggerExit( Collider other )
+	{
+		if( other.tag == "Player" )
+		{
+			doors.Close();
+		}
+	}
+}
