4641835bb07fa0886751ef2e1ef81ec105efddb7
Toby3011 <tobiaszirngibl@gmx.de>
2015-07-01 21:13:30 +0200

    Added: #118 Added Jump-Scare
    Added a little Jump-Scare ;)

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/Spider.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spider : MonoBehaviour
+{
+
+    private GameObject player;
+    private AudioSource audio;
+    // Use this for initialization
+     private float fallSpeed = 8.0f;
+     private bool spiderFalling = false;
+
+     void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        audio = GetComponent<AudioSource>();
+    }
+
+     void Update()
+     {
+
+         if (spiderFalling)
+             transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+         
+     }
+
+    // Update is called once per frame
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            spiderFalling = true;
+            audio.Play();
+        }
+    }
+}
