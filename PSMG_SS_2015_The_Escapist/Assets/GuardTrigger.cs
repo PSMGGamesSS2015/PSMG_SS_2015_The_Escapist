c105a6516579f088512589c324841b18659a2af9
Toby3011 <tobiaszirngibl@gmx.de>
2015-07-06 20:23:03 +0200

    Update: #124 Trigger to make enemy walk
    Changed the trigger to an static trigger in the enviroment

---
+++ PSMG_SS_2015_The_Escapist/Assets/GuardTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardTrigger : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject enemy;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            enemy.GetComponent<FollowAI>().startGoing = true;
+        }
+
+    }
+}
