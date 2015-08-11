e52c96bbc59cf6da9ea968d35879f51ecbbe93fc
Toby3011 <tobiaszirngibl@gmx.de>
2015-08-11 17:07:07 +0200

    Added: #144 Crosshair
    Added new Scene with crosshair

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+
+    public Texture2D crosshairTexture;
+    public float crosshairScale = 0.1f;
+	// Use this for initialization
+    void OnGUI()
+    {
+
+        if (Time.timeScale != 0)
+        {
+            if (crosshairTexture != null)
+                GUI.DrawTexture(new Rect((Screen.width - crosshairTexture.width * crosshairScale) / 2, (Screen.height - crosshairTexture.height * crosshairScale) / 2, crosshairTexture.width * crosshairScale, crosshairTexture.height * crosshairScale), crosshairTexture);
+        }
+    }
+}
