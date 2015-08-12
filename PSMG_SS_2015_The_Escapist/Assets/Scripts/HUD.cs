0703e4f8de2645173a4432d11b06853ba94f8bfe
Toby3011 <tobiaszirngibl@gmx.de>
2015-08-12 21:40:41 +0200

    Added: #146 Lockpicking Menu
    Added first design of lockpicking menu

--- PSMG_SS_2015_The_Escapist/Assets/Scripts/HUD.cs
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/HUD.cs
@@ -3,38 +3,76 @@ using System.Collections;
 
 public class HUD : MonoBehaviour {
 
-    private string bottleText, stoneText, gearwheelText, pipeText;
+    private string bottleText, stoneText, gearwheelText, pipeText, text;
     private int test = 3;
-    public Texture2D bottle, stone, gearwheel, pipe;
+    public Texture2D bottle, stone, gearwheel, pipe, closedlock, openlock;
+
+    private bool showInv, showLock;
+
+    public bool showText;
 	// Use this for initialization
 	void Start () {
-        
-        
+        showText = false;
+        showInv = true;
+        showLock = true;
+        text = "Wo bin ich?";
+        bottleText = "" + test;
+        stoneText = "" + test;
+        gearwheelText = "" + test;
+        pipeText = "" + test;
 	}
 	
 	// Update is called once per frame
 	void Update () {
        
-	   bottleText = "" + test;
-       stoneText = "" + test;
-       gearwheelText = "" + test;
-       pipeText = "" + test;
+
        
 	}
 
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 600, 50, 50), bottle); 
-        GUI.Label(new Rect(35, 620, 50, 30), bottleText);
 
-        GUI.Label(new Rect(50, 605, 50, 50), stone);
-        GUI.Label(new Rect(100, 620, 50, 30), stoneText);
+        inventory();
+        lockpicking();
+        texting();
 
-        GUI.Label(new Rect(120, 608, 35, 50), gearwheel);
-        GUI.Label(new Rect(165, 620, 50, 30), gearwheelText);
+    }
 
-        GUI.Label(new Rect(180, 608, 35, 50), pipe);
-        GUI.Label(new Rect(220, 620, 50, 30), pipeText);
+    private void texting()
+    {
+        if (showText)
+        {
+            GUI.TextArea(new Rect(Screen.width / 2 - 130, Screen.height / 2 + 200, 280, 50), text);
+        }
+    }
 
+    private void lockpicking()
+    {
+        if (showLock)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 130, Screen.height / 2 + 50, 280, 50), "");
+            GUI.Label(new Rect(Screen.width/2 - 120, Screen.height/2 + 50, 50, 50), openlock);
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 50, 50, 50), openlock);
+            GUI.Label(new Rect(Screen.width / 2 + 20, Screen.height / 2 + 50, 50, 50), openlock);
+            GUI.Label(new Rect(Screen.width / 2 + 90, Screen.height / 2 + 50, 50, 50), openlock);
+        }
+    }
+
+    private void inventory()
+    {
+        if (showInv)
+        {
+            GUI.Label(new Rect(10, 600, 50, 50), bottle);
+            GUI.Label(new Rect(35, 620, 50, 30), bottleText);
+
+            GUI.Label(new Rect(50, 605, 50, 50), stone);
+            GUI.Label(new Rect(100, 620, 50, 30), stoneText);
+
+            GUI.Label(new Rect(120, 608, 35, 50), gearwheel);
+            GUI.Label(new Rect(165, 620, 50, 30), gearwheelText);
+
+            GUI.Label(new Rect(180, 608, 35, 50), pipe);
+            GUI.Label(new Rect(220, 620, 50, 30), pipeText);
+        }
     }
 }
