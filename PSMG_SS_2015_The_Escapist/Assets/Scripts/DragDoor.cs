c75fe25dd2b29a41dbc6c2f0ce94934cb4417ba8
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-08-14 04:19:32 +0200

    Added: #122 Drag Scripts
    Split the drag functionality into 2 different scripts. One is only for doors, as the logic therefore is quite different compared to dragging objects.

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/DragDoor.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+public class DragDoor : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public float maxDragDistance = 2f;
+    public int numOfRays = 5;
+    public float fieldOfFocus = 30;
+    public float minDoorAngle = 0;
+    public float maxDoorAngle = 90;
+    public float dragAcceleration = 2f;
+    public float xSensitivity = 1f;
+    public float ySensitivity = 0.5f;
+    public float playerRotationCorrection = 90f;
+
+    private GameObject player;
+    private Camera firstPersonCam;
+    private float offsetAngle = 0;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        firstPersonCam = player.GetComponentInChildren<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!Input.GetButton("Use")) { return; }
+
+        GameObject focusedDoor = LookForDoorInPlayersFocus();
+        if (focusedDoor) Drag(focusedDoor);
+    }
+
+    private GameObject LookForDoorInPlayersFocus()
+    {
+        // RAYS POINTING AROUND PLAYER'S FOCUSPOINT WITHIN THE FOCUS FIELD
+        float focusFieldDiameter = fieldOfFocus / firstPersonCam.fieldOfView;
+        float stepSize = focusFieldDiameter / (numOfRays - 1);
+        float leftBorder = 0.5f - (focusFieldDiameter / 2);
+        
+        for (int rayNum = 0; rayNum < numOfRays; rayNum++)
+        {
+            Ray ray = firstPersonCam.ViewportPointToRay(new Vector3(leftBorder + (rayNum * stepSize), 0.5f, 0));
+            
+            // MAKE RAY VISIBLE FOR DEBUGING
+            Debug.DrawRay(ray.origin, ray.direction * maxDragDistance, Color.magenta);
+
+            GameObject focusedDoor;
+            if (focusedDoor = LookForDoor(ray)) { return focusedDoor; }
+        }
+
+        return null;
+    }
+
+    private GameObject LookForDoor(Ray ray)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxDragDistance, layerMask))
+        {
+            // GET RENDERER OF OBJECT HIT
+            Renderer hitRenderer = hit.collider.GetComponent<Renderer>();
+
+            // RETURN NULL IF RENDERER NOT TAGGED AS DOOR
+            if (hitRenderer.tag != "Door") { return null; }
+            
+            GameObject focusedDoor = hit.collider.gameObject;
+            return focusedDoor;
+        }
+
+        // RETURN NULL IF NO OBJECT FOUND
+        return null;
+    }
+
+    private void Drag(GameObject focusedDoor)
+    {
+        // CALC ANGLE FROM MOUSE MOVEMENT AND PLAYER ORIENTATION
+        float mouseAcceleration = ((Input.GetAxis("Mouse X") * xSensitivity) + (Input.GetAxis("Mouse Y") * ySensitivity));
+        float angle = mouseAcceleration * dragAcceleration * calcDragDirectionFactor(focusedDoor);
+
+        // CHECK IF ROTATION IS INSIDE DEFINED BOUNDARIES
+        float doorRot = focusedDoor.transform.localEulerAngles.y;
+        if (doorRot + angle <= minDoorAngle || doorRot + angle >= maxDoorAngle) { return; }
+
+        focusedDoor.transform.Rotate(Vector3.up, angle);
+    }
+
+    private int calcDragDirectionFactor(GameObject focusedDoor)
+    {
+        // THE IDEA OF THIS METHOD IS TO INTRODUCE A REVERSE FACTOR FOR THE MOUSE DRAG DIRECTION. 
+        // THE DRAG DIRECTION IS BASED ON THE ORIENTATION OF THE PLAYER TOWARDS THE AXIS OF THE OPEN DOOR.
+        // THE SWITCH POINT IS MARKED BY THE ROTATION THE PLAYER HAS, WHEN HE STANDS PARALLEL TO THE 90° OPENED DOOR.
+
+        float doorwayRot = focusedDoor.transform.parent.eulerAngles.y;
+        float playerRot = player.transform.eulerAngles.y;
+
+        // ORIENTATION OF THE PLAYER TOWARDS THE OPEN DOOR AXIS. THE RANGE OF VALUES OF THIS VARIABLE REACH FROM -180° TO 180°.
+        float relativePlayerOrientation = 180 - NormalizeAngle((playerRot - doorwayRot) + 180);
+
+        // FACTOR TO REVERSE DRAG ROTATION IF PLAYER LOOKS IN NEGATIVE DIRECTION OF THE OPEN DOOR AXIS
+        int dragDirectionFactor = (relativePlayerOrientation < 0) ? -1 : 1;
+
+        return dragDirectionFactor;
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        return angle % 360;
+    }
+}
---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/DragObject.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public float maxDragDistance = 2f;
+
+    public float k_Spring = 50.0f;
+    public float k_Damper = 5.0f;
+    public float k_Drag = 10.0f;
+    public float k_AngularDrag = 5.0f;
+    public float k_Distance = 0.2f;
+    public bool k_AttachToCenterOfMass = false;
+
+    private SpringJoint m_SpringJoint;
+    private GameObject player;
+    private Camera firstPersonCam;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        firstPersonCam = player.GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        // Make sure the user pressed the mouse down
+        if (!Input.GetMouseButtonDown(0)) { return; }
+
+        // We need to actually hit an object
+        RaycastHit hit = new RaycastHit();
+        Ray ray = firstPersonCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Debug.DrawRay(ray.origin, ray.direction * maxDragDistance, Color.green);
+
+        if (!Physics.Raycast(ray, out hit, maxDragDistance, layerMask)) { return; }
+
+        // GET RENDERER OF OBJECT HIT
+        Renderer hitRenderer = hit.collider.GetComponent<Renderer>();
+
+        // MAKE SURE WE HIT NO DOOR
+        if (hitRenderer.tag == "Door") { return; }
+
+        // We need to hit a rigidbody that is not kinematic
+        if (!hit.rigidbody || hit.rigidbody.isKinematic) { return; }
+
+
+        if (!m_SpringJoint)
+        {
+            var go = new GameObject("Rigidbody dragger");
+            Rigidbody body = go.AddComponent<Rigidbody>();
+            m_SpringJoint = go.AddComponent<SpringJoint>();
+            body.isKinematic = true;
+        }
+
+        m_SpringJoint.transform.position = hit.point;
+        m_SpringJoint.anchor = Vector3.zero;
+
+        m_SpringJoint.spring = k_Spring;
+        m_SpringJoint.damper = k_Damper;
+        m_SpringJoint.maxDistance = k_Distance;
+        m_SpringJoint.connectedBody = hit.rigidbody;
+
+        StartCoroutine("Drag", hit.distance);
+    }
+
+
+    private IEnumerator Drag(float distance)
+    {
+        var oldDrag = m_SpringJoint.connectedBody.drag;
+        var oldAngularDrag = m_SpringJoint.connectedBody.angularDrag;
+        m_SpringJoint.connectedBody.drag = k_Drag;
+        m_SpringJoint.connectedBody.angularDrag = k_AngularDrag;
+
+        while (Input.GetMouseButton(0))
+        {
+            var ray = firstPersonCam.ViewportPointToRay(new Vector3(0.5f, 0.9f, 0));
+            m_SpringJoint.transform.position = ray.GetPoint(distance);
+            yield return null;
+        }
+        if (m_SpringJoint.connectedBody)
+        {
+            m_SpringJoint.connectedBody.drag = oldDrag;
+            m_SpringJoint.connectedBody.angularDrag = oldAngularDrag;
+            m_SpringJoint.connectedBody = null;
+        }
+    }
+
+
+    private Camera FindCamera()
+    {
+        if (GetComponent<Camera>())
+        {
+            return GetComponent<Camera>();
+        }
+
+        return Camera.main;
+    }
+}
+
