9e4916db9507e8dfbc93f8c76ca6bac52d955217
Chris-shari <gimlorn.lrt@gmail.com>
2015-08-15 23:55:15 +0200

    Added: #105 Sounds 3/3

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/AudioScripts/EnemyKnightAudioController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyKnightAudioController : MonoBehaviour {
+
+    public AudioClip attackSound;
+    public AudioClip chasingSound;
+    public AudioClip detectedSound;
+
+    private AudioSource audioSrc;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        audioSrc = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+        if (!audioSrc.isPlaying)
+        {
+
+            if (anim.GetBool("IsChasing") == true)
+            {
+
+                audioSrc.clip = chasingSound;
+                audioSrc.Play();
+
+            }
+            else if (anim.GetBool("IsAttacking") == true)
+            {
+
+                audioSrc.clip = attackSound;
+                audioSrc.Play();
+
+            }
+        }
+	}
+}
---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/AudioScripts/MainMusicController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMusicController : MonoBehaviour {
+
+    public AudioClip theme1;
+    private AudioSource audioSrc;
+
+
+	// Use this for initialization
+	void Start () {
+
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (!audioSrc.isPlaying)
+        {
+            audioSrc.clip = theme1;
+            audioSrc.Play();
+
+        }
+
+
+	}
+}
