12dc496bb5648b8e11f6ebf66b3f937fc45db9c3
Toby3011 <tobiaszirngibl@gmx.de>
2015-08-18 17:41:58 +0200

    Added: #157 Pause menu script
    Added pause menu script. In the menu, you can select "Resume", "Main
    Menu" and "Quit"

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private bool paused = false;
+    private GUIStyle textStyle;
+    private GUIStyle titleStyle;
+
+    public Font myFont;
+
+    void Start()
+    {
+        textStyle = new GUIStyle();
+        textStyle.normal.textColor = Color.white;
+        textStyle.fontSize = 80;
+        textStyle.font = myFont;
+        textStyle.alignment = TextAnchor.MiddleCenter;
+
+
+        titleStyle = new GUIStyle();
+        titleStyle.normal.textColor = Color.white;
+        titleStyle.fontSize = 200;
+        titleStyle.font = myFont;
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+    }
+
+    void OnGUI()
+    {
+
+
+
+        if (paused == true)
+        {
+
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+
+            GUI.Label(new Rect(Screen.width / 2, 100, 100, 100), "Pause", titleStyle);
+            if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 -100, 80, 20), "Resume", textStyle))
+            {
+                paused = false;
+
+            }
+            if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 80, 20), "Main Menu", textStyle))
+            {
+                Application.LoadLevel(0);
+            }
+            if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 100, 80, 20), "Quit", textStyle))
+            {
+                Application.Quit();
+            }
+        }
+        if (paused == true)
+        {
+            Time.timeScale = 0;
+        }
+        if (paused == false)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
