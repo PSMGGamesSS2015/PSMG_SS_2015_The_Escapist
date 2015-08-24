44e75d166b729e97c6ec72e3d57baec4631f377d
Bene <Benedikt.Hierl@gmx.de>
2015-08-24 21:26:13 +0200

    Added: #187 LevelChanger Script + Prefab
    This script will switch from level 1 to level 2 to the outro., depending on which level you are at the moment.
    To make this script work the level need to be named as follows:
    
    - Level_1_Prison
    - Level_2_Canalisation
    - Outro
    
    The levelswitcher-prefab can be found at "Assets\3D\Player\LevelChanger.prefab"

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Whenever you have finished Level 1 go to Level 2
+        if (Application.loadedLevelName == "Level_1_Prison")
+        {
+            //Play a "finish level" sound
+            GetComponent<AudioSource>().Play();
+            //Load the next level
+            Application.LoadLevel("Level_2_Canalisation");
+
+            //Whenever you have finished level 2 go to the outro-screen
+        }
+        else if (Application.loadedLevelName == "Level_2_Canalisation")
+        {
+            //Play a "finish level" sound
+            GetComponent<AudioSource>().Play();
+            //Load the next level
+            Application.LoadLevel("Outro");
+        }
+        else
+        {
+            //Play a "finish level" sound
+            GetComponent<AudioSource>().Play();
+            //Debug message, which is, hopefully, never needed
+            Debug.Log("LevelSwitch: Ooops.. Normally I should not appear.. Please check the correct level order - because I wanted to load a level which does not exist (anymore?)");
+        }
+    }
+}
