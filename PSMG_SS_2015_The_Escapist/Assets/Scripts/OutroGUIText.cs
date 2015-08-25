55a298cd8c692476ae04af1dc11cca51efa3c5a3
Bene <Benedikt.Hierl@gmx.de>
2015-08-25 14:07:13 +0200

    Changed: #188 Outro now with text
    I have added story text to the outro and added the credits there.
    Works fine on my system - hope it does the same for you too

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/OutroGUIText.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OutroGUIText : MonoBehaviour
+{
+
+    private Text txtRef;
+
+    private string text_0 = "";
+
+    private string text_1 = "Gerlin finally found out why all of this happened...";
+    private string text_2 = "She should have been part in an insane experiment";
+    private string text_3 = "An evil experiment of the crazy knights from the Gnarlekh' Clan";
+    private string text_4 = "The more she read in the book the better she knew there is only thing to do...:";
+    private string text_5 = "To get the hell out of here!";
+    private string text_6 = "She saw the opening doors flooded by divine light";
+    private string text_7 = "Gerlin had to get there without attracting any attention";
+    private string text_8 = "So Gerlin did one step after another and tried to be as silent as possible";
+    private string text_9 = "By watching the opening doors a warm smooth feeling came up on her body";
+    private string text_10 = "Hope! There is finally hope to escape this madness!";
+    private string text_11 = "Nobody recognized her - the way is free";
+    private string text_12 = "Now go, Gerlin, go! You can finally ESCAPE !";
+
+    private string credits_1 = "CREDITS:";
+    private string credits_2 = "... developed by ...";
+
+    private string credits_3 = "... Cristoph Herbert ...";
+    private string credits_4 = "... Julien Wachter ...";
+    private string credits_5 = "... Tobias Zirngibl ...";
+    private string credits_6 = "... Daniel Hecht ...";
+    private string credits_7 = "... Oliver Poeppel ...";
+    private string credits_8 = "... Benedikt Hierl ...";
+
+    private string credits_9 = "Thank you for playing";
+    private string credits_10 = "... Game development @ Uni Regensburg in SS15 ...";
+    private string credits_11 = "GAME OVER";
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //References the Text-Object
+        txtRef = GetComponent<Text>();
+        StartCoroutine(SetGUIText());
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SetGUIText()
+    {
+
+
+        //Change text color to white
+        txtRef.material.color = Color.white;
+
+        //Display story texts
+        txtRef.text = text_1;
+        yield return new WaitForSeconds(5.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_2;
+        yield return new WaitForSeconds(5.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_3;
+        yield return new WaitForSeconds(5.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_4;
+        yield return new WaitForSeconds(5.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.5f);
+
+        txtRef.text = text_5;
+        yield return new WaitForSeconds(7.5f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_6;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_7;
+        yield return new WaitForSeconds(8.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(6.0f);
+
+        txtRef.text = text_8;
+        yield return new WaitForSeconds(12.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(5.0f);
+
+        txtRef.text = text_9;
+        yield return new WaitForSeconds(7.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_10;
+        yield return new WaitForSeconds(5.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(2.0f);
+
+        txtRef.text = text_11;
+        yield return new WaitForSeconds(3.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = text_12;
+        yield return new WaitForSeconds(6.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(16.5f);
+
+
+        //Display credits texts here
+
+        //At first change the color to black to make it readable
+        txtRef.material.color = Color.black;
+
+        txtRef.text = credits_1;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_2;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_3;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_4;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_5;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_6;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_7;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_8;
+        yield return new WaitForSeconds(4.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_9;
+        yield return new WaitForSeconds(3.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(1.0f);
+
+        txtRef.text = credits_10;
+        yield return new WaitForSeconds(3.0f);
+        txtRef.text = text_0;
+        yield return new WaitForSeconds(6.8f);
+
+
+        //Change the text color to red to make it look final
+        txtRef.material.color = Color.red;
+
+        txtRef.text = credits_11;
+    }
+}
