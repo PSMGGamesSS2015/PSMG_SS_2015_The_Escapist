143323bc77a5eb3f43e03ab03d1f1ea536e9593f
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-08-26 03:32:30 +0200

    Added: Light Flickering Skript
    For Audio Guru Chris, to make use of his giant sound library ;)

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/LightFlickering.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlickering : MonoBehaviour {
+
+    private Light light;
+
+    float minFlickerTime = 0.01f;
+    float maxFlickerTime = 1f;
+    float minLightIntensity = 3f;
+    float maxLightIntensity = 3.5f;
+
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine("flicker");
+    }
+
+    IEnumerator flicker()
+    {
+        while(true)
+        {
+            light.enabled = true;
+            light.intensity = Random.Range(minLightIntensity, maxLightIntensity);
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+            light.enabled = false;
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+        }
+    }
+}
