c21fe6413577b85265b450d5eb8775ce1f8cdfd1
Bene <Benedikt.Hierl@gmx.de>
2015-08-29 03:10:37 +0200

    Changed: #200 Code of LevelSwitch is more dynamic
    Instead of comparing Strings the Indexnumbers will be compared and therefore we are way more flexible by using this.

--- PSMG_SS_2015_The_Escapist/Assets/Scripts/Misc/LevelSwitch.cs
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/Misc/LevelSwitch.cs
@@ -16,31 +16,10 @@ public class LevelSwitch : MonoBehaviour
 
     }
 
+    // As soon as the player collides with it load the next level
     void OnCollisionEnter(Collision col)
     {
-        //Whenever you have finished Level 1 go to Level 2
-        if (Application.loadedLevelName == "Level_1_Prison")
-        {
-            //Play a "finish level" sound
-            GetComponent<AudioSource>().Play();
-            //Load the next level
-            Application.LoadLevel("Level_2_Canalisation");
-
-            //Whenever you have finished level 2 go to the outro-screen
-        }
-        else if (Application.loadedLevelName == "Level_2_Canalisation")
-        {
-            //Play a "finish level" sound
-            GetComponent<AudioSource>().Play();
-            //Load the next level
-            Application.LoadLevel("Outro");
-        }
-        else
-        {
-            //Play a "finish level" sound
-            GetComponent<AudioSource>().Play();
-            //Debug message, which is, hopefully, never needed
-            Debug.Log("LevelSwitch: Ooops.. Normally I should not appear.. Please check the correct level order - because I wanted to load a level which does not exist (anymore?)");
-        }
+        GetComponent<AudioSource>().Play();
+        Application.LoadLevel(Application.loadedLevel+1);
     }
 }
