e1a01eac5f5183c0b8604474048ce4f4082c28bc
julien-ur <julien.wachter@stud.uni-regensburg.de>
2015-09-01 20:13:23 +0200

    Fixed: Bugs Level 1

---
+++ PSMG_SS_2015_The_Escapist/Assets/Scripts/UI/TextActivateFocused.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextActivateFocused : InteractiveObject {
+
+    //
+    // Script for displaying story text
+    //
+
+    public string text;
+
+    private enum States { active = 0, inactive = 1 }
+    private States state = States.active;
+
+    public override void trigger()
+    {
+        if(state == States.active)
+        {
+            GameObject.Find("HUD").GetComponent<UIText>().showText(text);
+            state = States.inactive;
+        }
+    }
+
+    public override int getState()
+    {
+        return (int)(state);
+    }
+
+    public override bool hasInteractions()
+    {
+        return (state == States.active);
+    }
+}
